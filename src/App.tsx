9338e5cc51b2219111f7a74d0eec0ded1eee8678
Pedro Paulo <pedropaulo@interlinksistemas.com.br>
2026-03-12 02:02:42 -0300

    feat: adiciona fluxo de submit e tela simples de resposta

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,47 @@ import bg from "./assets/imgs/bg_Coding-Conf.png";
 import { Header } from "./components/header";
 import { About } from "./components/about";
 import { Inputs } from "./components/inputs";
+
+import { useState } from "react";
+export type InputsData = {
+  fullName: string;
+  email: string;
+  github: string;
+  image: File | null;
+};
+
 function App() {
+  const [submitedData, setSubmitedData] = useState<InputsData | null>(null);
+
+  function handleSubmit(data: InputsData) {
+    setSubmitedData(data);
+  }
+
+   function handleBack() {
+    setSubmitedData(null);
+  }
+
+  if (submitedData) {
+    return (
+      <section>
+        <h1>Dados</h1>
+        <p>Nome: {submitedData.fullName}</p>
+        <p>Email: {submitedData.email}</p>
+        <p>GitHub: {submitedData.github}</p>
+        <button onClick={handleBack}>Voltar</button>
+      </section>
+    );
+  }
+
+
   return (
     <main
-      className="bg-cover min-h-full px-4 py-6 md:px-40 md:py-20 lg:px-72 xl:px-96 2xl:px-128"
+      className="bg-cover min-h-full px-4 py-6 md:px-40 md:py-20 lg:px-72 xl:px-96 2xl:px-128 cursor-default"
       style={{ backgroundImage: `url(${bg})` }}
     >
       <Header />
       <About />
-      <Inputs />
+      <Inputs onSubmit={handleSubmit} />
     </main>
   );
 }
--- src/components/inputs.tsx
+++ src/components/inputs.tsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
+import type { InputsData } from "../App";
+
 import upload from "../assets/imgs/upload.png";
 
-export function Inputs() {
+type InputsProps = {
+  onSubmit: (data: InputsData) => void;
+};
+
+export function Inputs({ onSubmit }: InputsProps) {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [github, setGithub] = useState("");
+  const [image, setImage] = useState<File | null>(null);
+
+  function handleFormSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    onSubmit({ fullName, email, github, image });
+  }
+
   return (
-    <section className="w-full space-y-4 font-poppins lg:text-md xl:text-xl">
+    <form
+      onSubmit={handleFormSubmit}
+      className="w-full space-y-4 font-poppins lg:text-md xl:text-xl"
+    >
       <div className="flex flex-col items-center justify-centerp-4">
         <div className="flex flex-col items-center justify-center border-2 border-dashed border-gray-300 rounded-md p-4 cursor-pointer bg-blue-800/50">
           <img src={upload} alt="Upload Avatar" className="h-12 mb-2" />
           <p className="text-white">Drag and drop or click to upload</p>
-          <input type="file" accept="image/*" className="bg-red-200 hidden" />
+          <input
+            type="file"
+            accept="image/*"
+            className="bg-red-200 hidden"
+            onChange={(e) => setImage(e.target.files?.[0] ?? null)}
+          />
         </div>
         <p className="text-xs text-gray-300 mt-2">
           Upload your photo (JPG or PNG, max size 500KB).
@@ -19,6 +44,8 @@ export function Inputs() {
         <input
           type="text"
           placeholder="Enter your full name"
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
           className="w-full p-2 rounded-md border bg-black/50 border-gray-300 focus:outline-none focus:ring-2 focus:ring-white placeholder:text-gray-300"
         />
       </div>
@@ -28,6 +55,8 @@ export function Inputs() {
         <input
           type="email"
           placeholder="example@email.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           className="w-full p-2 rounded-md border bg-black/50 border-gray-300 focus:outline-none focus:ring-2 focus:ring-white placeholder:text-gray-300"
         />
       </div>
@@ -37,13 +66,18 @@ export function Inputs() {
         <input
           type="text"
           placeholder="@yourusername"
+          value={github}
+          onChange={(e) => setGithub(e.target.value)}
           className="w-full p-2 rounded-md border bg-black/50 border-gray-300 focus:outline-none focus:ring-2 focus:ring-white placeholder:text-gray-300"
         />
       </div>
 
-      <button className="w-full p-3 rounded-md bg-orange-400 text-blue-950 font-bold text-center text-lg hover:bg-orange-500 transition-colors">
+      <button
+        type="submit"
+        className="w-full p-3 rounded-md bg-orange-400 text-blue-950 font-bold text-center text-lg hover:bg-orange-500 transition-colors cursor-pointer"
+      >
         Generate My Ticket
       </button>
-    </section>
+    </form>
   );
 }
